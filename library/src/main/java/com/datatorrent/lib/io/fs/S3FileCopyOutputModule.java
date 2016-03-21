5a815c87451b7a7ec55f614571f4ab5743cd826e
Chaitanya <chaitanya@datatorrent.com>
2016-03-21 22:54:14 +0530

    Implemented S3 Copy Module

--- library/src/main/java/com/datatorrent/lib/io/fs/S3FileCopyOutputModule.java
+++ library/src/main/java/com/datatorrent/lib/io/fs/S3FileCopyOutputModule.java
@@ -1,7 +1,15 @@
 package com.datatorrent.lib.io.fs;
 
+import com.esotericsoftware.kryo.NotNull;
+
 public class S3FileCopyOutputModule extends FSCopyOutputModule
 {
+  @NotNull
+  private String accessKey;
+  @NotNull
+  private String secretKey;
+  @NotNull
+  private String bucketName;
   @Override
   public BlockWriter createBlockWriter()
   {
@@ -17,6 +25,41 @@ public class S3FileCopyOutputModule extends FSCopyOutputModule
   @Override
   public FileMerger createFileMerger()
   {
-    return new S3FileMerger();
+    S3FileMerger s3Merger = new S3FileMerger();
+    s3Merger.setAccessKey(accessKey);
+    s3Merger.setSecretKey(secretKey);
+    s3Merger.setBucketName(bucketName);
+    s3Merger.setDirectoryName(outputDirectoryPath);
+    return s3Merger;
+  }
+
+  public String getAccessKey()
+  {
+    return accessKey;
+  }
+
+  public void setAccessKey(String accessKey)
+  {
+    this.accessKey = accessKey;
+  }
+
+  public String getSecretKey()
+  {
+    return secretKey;
+  }
+
+  public void setSecretKey(String secretKey)
+  {
+    this.secretKey = secretKey;
+  }
+
+  public String getBucketName()
+  {
+    return bucketName;
+  }
+
+  public void setBucketName(String bucketName)
+  {
+    this.bucketName = bucketName;
   }
 }
--- library/src/main/java/com/datatorrent/lib/io/fs/S3FileMerger.java
+++ library/src/main/java/com/datatorrent/lib/io/fs/S3FileMerger.java
@@ -3,14 +3,46 @@ package com.datatorrent.lib.io.fs;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.Path;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.esotericsoftware.kryo.NotNull;
+
+import com.datatorrent.api.Context;
+import com.datatorrent.api.DAG;
 
 public class S3FileMerger extends FileMerger
 {
-  protected static final String S3_TMP_DIR = "s3";
+  private static final Logger logger = LoggerFactory.getLogger(S3FileMerger.class);
+  protected static final String S3_TMP_DIR = "S3TmpFiles";
 
   protected transient Configuration conf;
+  @NotNull
+  private String accessKey;
+  @NotNull
+  private String secretKey;
+  @NotNull
+  private String bucketName;
+  @NotNull
+  private String directoryName;
+  protected transient AmazonS3 s3client;
+
+  @Override
+  public void setup(Context.OperatorContext context)
+  {
+    s3client = new AmazonS3Client(new BasicAWSCredentials(accessKey,secretKey));
+    filePath = context.getValue(DAG.APPLICATION_PATH) + Path.SEPARATOR + S3_TMP_DIR;
+    super.setup(context);
+  }
 
   /**
    * S3 supports rename functionality. But, it effects the performance. Because, rename does the following steps:
@@ -36,5 +68,54 @@ public class S3FileMerger extends FileMerger
   @Override
   protected void moveToFinalFile(Synchronizer.OutputFileMetadata outFileMetadata) throws IOException
   {
+    try {
+      FSDataInputStream fsinput = appFS.open(tempOutFilePath);
+      ObjectMetadata omd = new ObjectMetadata();
+      omd.setContentLength(outFileMetadata.getFileLength());
+      String keyName = directoryName + Path.SEPARATOR + outFileMetadata.getStitchedFileRelativePath();
+      s3client.putObject(new PutObjectRequest(bucketName, keyName, fsinput, omd));
+    } catch (IOException e) {
+      logger.error("Unable to create Stream: {}", e.getMessage());
+    }
+  }
+
+  public String getAccessKey()
+  {
+    return accessKey;
+  }
+
+  public void setAccessKey(String accessKey)
+  {
+    this.accessKey = accessKey;
+  }
+
+  public String getSecretKey()
+  {
+    return secretKey;
+  }
+
+  public void setSecretKey(String secretKey)
+  {
+    this.secretKey = secretKey;
+  }
+
+  public String getBucketName()
+  {
+    return bucketName;
+  }
+
+  public void setBucketName(String bucketName)
+  {
+    this.bucketName = bucketName;
+  }
+
+  public String getDirectoryName()
+  {
+    return directoryName;
+  }
+
+  public void setDirectoryName(String directoryName)
+  {
+    this.directoryName = directoryName;
   }
 }
