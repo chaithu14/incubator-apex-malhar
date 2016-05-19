74c390e7c64a3d50caba0fca12f4d53449ec2cf8
Chaitanya <chaitanya@datatorrent.com>
2016-05-19 17:25:46 +0530

    Updated version

--- library/src/main/java/com/datatorrent/lib/join/ManagedStateStore.java
+++ library/src/main/java/com/datatorrent/lib/join/ManagedStateStore.java
@@ -1,6 +1,5 @@
 package com.datatorrent.lib.join;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,20 +8,16 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.apex.malhar.lib.state.managed.ManagedTimeStateImpl;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
-
 import com.datatorrent.api.Context;
 import com.datatorrent.api.DAG;
+import com.datatorrent.lib.codec.KryoSerializableStreamCodec;
 import com.datatorrent.lib.fileaccess.FileAccessFSImpl;
 import com.datatorrent.netlet.util.Slice;
 
 public class ManagedStateStore extends ManagedTimeStateImpl implements JoinStore
 {
-
   private static int i = 0;
-  private final transient Kryo kryo = new Kryo();
+  private final transient KryoSerializableStreamCodec kryo = new KryoSerializableStreamCodec();
   private transient Class outputClass;
   private String outputClassStr = "com.datatorrent.lib.join.TimeEventImpl";
   @Override
@@ -30,18 +25,13 @@ public class ManagedStateStore extends ManagedTimeStateImpl implements JoinStore
   {
     TimeEvent te = (TimeEvent)tuple;
     Object key = te.getEventKey();
-    ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
-    Output output1 = new Output(bos1);
-    kryo.writeObject(output1, key);
-    output1.close();
-    LOG.info("getValid Tuple - 1: {} -> {}", key, bos1.toByteArray());
-    Slice value = super.getSync(0, new Slice(bos1.toByteArray()));
+
+    LOG.info("getValid Tuple - 1: {} -> {}", key);
+    Slice value = super.getSync(0, kryo.toByteArray(key));
     List<Object> output = new ArrayList<>();
     if (value != null) {
       LOG.info("getValid Tuple - 1: {} -> {}", key,value.buffer);
-      Input lInput = new Input(value.buffer);
-      //output.add(kryo.readObject(lInput, outputClass));
-      output.add(kryo.readClassAndObject(lInput));
+      output.add(kryo.fromByteArray(value));
       LOG.info("getValid Tuple - 2: {}", key);
     }
     LOG.info("getValid Tuple - 3: {}", key);
@@ -54,16 +44,7 @@ public class ManagedStateStore extends ManagedTimeStateImpl implements JoinStore
     TimeEvent te = (TimeEvent)tuple;
     Object key = te.getEventKey();
     Object value = te.getValue();
-    ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
-    Output output1 = new Output(bos1);
-    kryo.writeObject(output1, key);
-    output1.close();
-    ByteArrayOutputStream bos2 = new ByteArrayOutputStream();
-    Output output2 = new Output(bos2);
-    kryo.writeClassAndObject(output2, tuple);
-    output2.close();
-    super.put(0,te.getTime(),new Slice(bos1.toByteArray()), new Slice(bos2.toByteArray()));
-    LOG.info("Put Object: {}", bos1.toByteArray());
+    super.put(0,te.getTime(),kryo.toByteArray(key), kryo.toByteArray(tuple));
     return true;
   }
 
