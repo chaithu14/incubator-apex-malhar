7b374b5b8ef889a6bb7266a8b0cdc9e198b3a198
chaitanya <chaithu@apache.org>
2017-01-27 21:59:09 +0530

    Fixed the partitioning issue

--- library/src/main/java/org/apache/apex/malhar/lib/join/POJOInnerJoinOperator.java
+++ library/src/main/java/org/apache/apex/malhar/lib/join/POJOInnerJoinOperator.java
@@ -19,7 +19,10 @@
 package org.apache.apex.malhar.lib.join;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.ClassUtils;
@@ -27,10 +30,13 @@ import org.apache.commons.lang3.ClassUtils;
 import com.datatorrent.api.Context;
 import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.DefaultPartition;
 import com.datatorrent.api.Operator;
+import com.datatorrent.api.Partitioner;
 import com.datatorrent.api.StreamCodec;
 import com.datatorrent.api.annotation.InputPortFieldAnnotation;
 import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
+import com.datatorrent.lib.util.KryoCloneUtils;
 import com.datatorrent.lib.util.PojoUtils;
 
 /**
@@ -42,12 +48,13 @@ import com.datatorrent.lib.util.PojoUtils;
  * @since 3.5.0
  */
 @org.apache.hadoop.classification.InterfaceStability.Evolving
-public class POJOInnerJoinOperator extends AbstractManagedStateInnerJoinOperator<Object,Object> implements Operator.ActivationListener<Context>
+public class POJOInnerJoinOperator extends AbstractManagedStateInnerJoinOperator<Object,Object> implements Operator.ActivationListener<Context>, Partitioner<POJOInnerJoinOperator>
 {
   private transient long timeIncrement;
   private transient FieldObjectMap[] inputFieldObjects = (FieldObjectMap[])Array.newInstance(FieldObjectMap.class, 2);
   protected transient Class<?> outputClass;
   private long time = System.currentTimeMillis();
+  private int partitionCount = 1;
 
   @OutputPortFieldAnnotation(schemaRequired = true)
   public final transient DefaultOutputPort<Object> outputPort = new DefaultOutputPort<Object>()
@@ -231,6 +238,44 @@ public class POJOInnerJoinOperator extends AbstractManagedStateInnerJoinOperator
       return new JoinStreamCodec(getLeftKeyExpression());
     }
     return new JoinStreamCodec(getRightKeyExpression());
+  }
+
+  @Override
+  public Collection<Partition<POJOInnerJoinOperator>> definePartitions(Collection<Partition<POJOInnerJoinOperator>> partitions, PartitioningContext context)
+  {
+    boolean isInitialPartition = partitions.iterator().next().getStats() == null;
+    if (!isInitialPartition) {
+      // support for dynamic partitioning requires lineage tracking
+      return partitions;
+    }
+
+    final int numPartitions = DefaultPartition.getRequiredPartitionCount(context, this.partitionCount);
+    List<Partition<POJOInnerJoinOperator>> newPartitions = new ArrayList<>(numPartitions);
+
+    for (int i = 0; i < numPartitions; i++) {
+      Partition<POJOInnerJoinOperator> p = new DefaultPartition<>(KryoCloneUtils.cloneObject(this));
+      newPartitions.add(p);
+    }
+
+    DefaultPartition.assignPartitionKeys(newPartitions, input1);
+    DefaultPartition.assignPartitionKeys(newPartitions, input2);
+    return newPartitions;
+  }
+
+  @Override
+  public void partitioned(Map<Integer, Partition<POJOInnerJoinOperator>> partitions)
+  {
+
+  }
+
+  public int getPartitionCount()
+  {
+    return partitionCount;
+  }
+
+  public void setPartitionCount(int partitionCount)
+  {
+    this.partitionCount = partitionCount;
   }
 
   private class FieldObjectMap
