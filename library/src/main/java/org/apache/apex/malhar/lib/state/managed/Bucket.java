ab4b9551901dbdaf0d9ba0ed20192fe95fd7d29c
chaitanya <chaithu@apache.org>
2017-01-30 12:39:13 +0530

    Fixed the issue of validation

--- library/src/main/java/org/apache/apex/malhar/lib/state/managed/Bucket.java
+++ library/src/main/java/org/apache/apex/malhar/lib/state/managed/Bucket.java
@@ -489,6 +489,7 @@ public interface Bucket extends ManagedStateComponent, KeyValueByteStreamProvide
             reader.close();
           }
         }
+        cachedBucketMetas.clear();
       }
       cachedBucketMetas = null;
 
@@ -561,21 +562,27 @@ public interface Bucket extends ManagedStateComponent, KeyValueByteStreamProvide
 
           long memoryFreed = 0;
 
-          if (cachedBucketMetas != null) {
-
-            for (BucketsFileSystem.TimeBucketMeta tbm : cachedBucketMetas.values()) {
-              FileAccess.FileReader reader = readers.remove(tbm.getTimeBucketId());
-              if (reader != null) {
-                memoryFreed += tbm.getSizeInBytes();
-                try {
-                  reader.close();
-                } catch (IOException e) {
-                  throw new RuntimeException("closing reader " + bucketId + ", " + tbm.getTimeBucketId(), e);
+          for (BucketedValue bucketedValue : bucketData.values()) {
+            FileAccess.FileReader reader = readers.get(bucketedValue.getTimeBucket());
+            if (reader != null) {
+              //closing the file reader for the time bucket if it is in memory because the time-bucket is modified
+              //so it will be re-written by BucketsDataManager
+              try {
+                BucketsFileSystem.TimeBucketMeta tbm = cachedBucketMetas.get(bucketedValue.getTimeBucket());
+                if (tbm != null) {
+                  memoryFreed += tbm.getSizeInBytes();
                 }
+                LOG.debug("closing reader {} {}", bucketId, bucketedValue.getTimeBucket());
+                reader.close();
+              } catch (IOException e) {
+                throw new RuntimeException("closing reader " + bucketId + ", " + bucketedValue.getTimeBucket(), e);
               }
+              readers.remove(bucketedValue.getTimeBucket());
+            }
+            if (readers.isEmpty()) {
+              break;
             }
           }
-
           sizeInBytes.getAndAdd(-memoryFreed);
           if (!bucketData.isEmpty()) {
             LOG.info("Committed; {} -> {}", committedWindowId, ((FileAccessFSImpl)managedStateContext.getFileAccess()).getBasePath());
@@ -586,8 +593,6 @@ public interface Bucket extends ManagedStateComponent, KeyValueByteStreamProvide
           break;
         }
       }
-
-      cachedBucketMetas = null;
     }
 
     @Override
