95708493c2e3a2821e76439bd9b4cb3c4985e123
chaitanya <chaithu@apache.org>
2017-04-05 15:31:47 +0530

    APEXMALHAR-2459 1)Refactor the existing Kafka Input Operator. 2)Added the support of KafkaInputOperator using 0.10 consumer API

---
+++ kafka/kafka010/src/test/java/org/apache/apex/malhar/kafka/KafkaInputOperatorTest.java
@@ -0,0 +1,55 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.apex.malhar.kafka;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.DefaultOutputPort;
+
+/**
+ * A bunch of test to verify the input operator will be automatically partitioned
+ * per kafka partition This test is launching its
+ * own Kafka cluster.
+ */
+@RunWith(Parameterized.class)
+public class KafkaInputOperatorTest extends AbstractKafkaInputOperatorTest
+{
+
+  public KafkaInputOperatorTest(boolean hasMultiCluster, boolean hasMultiPartition, String partition)
+  {
+    super(hasMultiCluster, hasMultiPartition, partition);
+  }
+
+  KafkaSinglePortInputOperator node;
+
+  @Override
+  public AbstractKafkaInputOperator createAndAddKafkaOpToDAG(DAG dag, String testName)
+  {
+    node = dag.addOperator(testName, new KafkaSinglePortInputOperator());
+    return node;
+  }
+
+  @Override
+  public DefaultOutputPort getOutputPortOfKafka()
+  {
+    return node.outputPort;
+  }
+}
